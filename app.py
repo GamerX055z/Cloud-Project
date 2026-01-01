3916c8ca7fda8ae6355e1b65b581d24299c20b7c
Aditya Sukumaran <aditya.sukumaran2005@gmail.com>
2026-01-01 10:10:35 +0530

    Add basic Flask logging

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level=logging.INFO)
+
 @app.route("/")
 def home():
+    app.logger.info("Home page accessed")
     return render_template("index.html")
 
 @app.route("/about")
 def about():
+    app.logger.info("About page accessed")
     return render_template("about.html")
 
 if __name__ == "__main__":
